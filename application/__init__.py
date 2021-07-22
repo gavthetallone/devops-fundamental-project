834e6c075000749a8647519146f268a3cc4f3112
gavthetallone <gavinvw123@gmail.com>
2021-07-22 13:24:24 +0000

    GW: Set up a stand-alone sql database for the application

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,8 @@ import os
 app = Flask(__name__)
 
 app.config.update(
-    SQLALCHEMY_DATABASE_URI = "sqlite:///data.db",
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///data.db",
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:squareroot@35.230.137.217/data",
     SQLALCHEMY_TRACK_MODIFICATIONS=True,
     SECRET_KEY=str(os.urandom(16))
 )

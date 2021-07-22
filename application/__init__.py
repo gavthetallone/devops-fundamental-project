b079c308c6711a079f2541bbba894d8162eb818b
gavthetallone <gavinvw123@gmail.com>
2021-07-22 13:42:10 +0000

    GW: Updated '__init__.py' with getenv function

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from os import getenv
 
 app = Flask(__name__)
 
 app.config.update(
     # SQLALCHEMY_DATABASE_URI = "sqlite:///data.db",
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:squareroot@35.230.137.217/data",
+    SQLALCHEMY_DATABASE_URI = str(os.getenv('DATABASE_URI')),
     SQLALCHEMY_TRACK_MODIFICATIONS=True,
     SECRET_KEY=str(os.urandom(16))
 )
